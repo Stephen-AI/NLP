0f6b1b30101c874d89225f947dff5741a57986fe
Stephen Aigbomian <stephenaigbomian@gmail.com>
2020-02-08 11:44:37 -0600

    Cleaned up a1, init a2

---
+++ a2/models.py
@@ -0,0 +1,52 @@
+# models.py
+
+import torch
+import torch.nn as nn
+from torch import optim
+import numpy as np
+import random
+from sentiment_data import *
+
+
+class SentimentClassifier(object):
+    """
+    Sentiment classifier base type
+    """
+
+    def predict(self, ex_words: List[str]):
+        """
+        Makes a prediction on the given sentence
+        :param ex_words: words to predict on
+        :return: 0 or 1 with the label
+        """
+        raise Exception("Don't call me, call my subclasses")
+
+
+class TrivialSentimentClassifier(SentimentClassifier):
+    def predict(self, ex_words: List[str]):
+        """
+        :param ex:
+        :return: 1, always predicts positive class
+        """
+        return 1
+
+
+class NeuralSentimentClassifier(SentimentClassifier):
+    """
+    Implement your NeuralSentimentClassifier here. This should wrap an instance of the network with learned weights
+    along with everything needed to run it on new data (word embeddings, etc.)
+    """
+    def __init__(self):
+        raise NotImplementedError
+
+
+def train_deep_averaging_network(args, train_exs: List[SentimentExample], dev_exs: List[SentimentExample], word_embeddings: WordEmbeddings) -> NeuralSentimentClassifier:
+    """
+    :param args: Command-line args so you can access them here
+    :param train_exs: training examples
+    :param dev_exs: development set, in case you wish to evaluate your model during training
+    :param word_embeddings: set of loaded word embeddings
+    :return: A trained NeuralSentimentClassifier model
+    """
+    raise NotImplementedError
+
